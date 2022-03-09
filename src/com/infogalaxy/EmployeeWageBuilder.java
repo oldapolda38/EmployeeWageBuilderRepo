9c50f180ffdfed6731723946756b45d31c8d1c93
oldapolda38 <oldapolda38@gmail.com>
2022-03-09 12:21:01 +0530

    MODIFIED: Added Part Time and Full Time

--- src/com/infogalaxy/EmployeeWageBuilder.java
+++ src/com/infogalaxy/EmployeeWageBuilder.java
@@ -6,7 +6,8 @@ public class EmployeeWageBuilder {
     public static void main(String[] args) {
         //UC-2 Daily Wage Calculation
         final int WAGE_PER_HR = 20;
-        final int FULL_DAY_HR = 8;
+        final int FULL_TIME_HR = 8;
+        final int PART_TIME_HR = 4;
         int total_wage = 0;
         //UC-1 Employee Attendance
         Random random = new Random();
@@ -15,7 +16,14 @@ public class EmployeeWageBuilder {
             System.out.println("Employee is Absent.");
         } else {
             System.out.println("Employee is Present.");
-            total_wage = FULL_DAY_HR * WAGE_PER_HR;
+            int empType = random.nextInt()%2;
+            if(empType == 0) {
+                System.out.println("Employee is Part Time.");
+                total_wage = WAGE_PER_HR * PART_TIME_HR;
+            } else {
+                System.out.println("Employee is Full Time.");
+                total_wage = WAGE_PER_HR * FULL_TIME_HR;
+            }
         }
         System.out.println("Total wage = "+ total_wage);
     }
